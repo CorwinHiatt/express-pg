a342db17214beed5677b9c2a9243e62e0ef62e35
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-19 13:54:25 -0400

    4

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from 'express'
+import pg from 'pg'
+import { creds } from './creds.js'
+const { Pool } = pg
 
 const app = express()
 
 
-app.get('/customers', (req, res) => {
-    res.send('This will be a list of customers soon....🤯')
+app.get('/customers', async (req, res) => {
+    const pool = new Pool(creds)
+   const customers = await pool.query("SELECT * FROM customers")
+    res.send(customers)
 })
 
 app.listen(3030, () => console.log('Listening 0n http://localhost:3030....'))
