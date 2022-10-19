14da1f71ee033eccc06c84c14a2d9ad18cae52aa
Corwin Hiatt <corwinhiatt@yahoo.com>
2022-10-19 13:42:37 -0400

    3

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import express from 'express'
 const app = express()
 
 
-
+app.get('/customers', (req, res) => {
+    res.send('This will be a list of customers soon....🤯')
+})
 
 app.listen(3030, () => console.log('Listening 0n http://localhost:3030....'))
+
